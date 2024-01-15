2568a8460b50570e922315db46dad53ee8a9dcaf
Abhradip Saha <110524706+abhradip-saha@users.noreply.github.com>
2024-01-16 00:15:43 +0530

    Update image-controller.js

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -7,7 +7,7 @@ export const uploadImage = async (request, response)=> {
     }
     try{
         const file = await File.create(fileObj)
-        response.status(200).json({path : `http://localhost:8000/file/${file._id}`})
+        response.status(200).json({path : `https://abhradip-file.onrender.com/file/${file._id}`})
     }
         catch(error){
             console.error(error.message);
