0716ac566eea9cdca243a707814e6a55c9bef4fe
Abhradip Saha <110524706+abhradip-saha@users.noreply.github.com>
2024-01-16 00:14:35 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -2,15 +2,15 @@ import mongoose from 'mongoose';
 
 
 
-const DBConnection = async() =>{
+const DBConnection = () =>{
     const DB_URL=`mongodb://abhradip:saha@ac-h4pi8ti-shard-00-00.omvunhe.mongodb.net:27017,ac-h4pi8ti-shard-00-01.omvunhe.mongodb.net:27017,ac-h4pi8ti-shard-00-02.omvunhe.mongodb.net:27017/?ssl=true&replicaSet=atlas-un9j66-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    try{
-       await  mongoose.connect(DB_URL,{ useNewUrlParser:true });
-        console.log('Database connected successfully');
-    }catch(error){
-            console.error("Error while connecting database ",error.message);
-        }
-    
+    mongoose.connect(DB_URL)
+   .then(()=>{
+      console.log("DB connection successful.");
+   })
+   .catch((err)=>{
+      console.log(`DB connection error:${err}`);
+   });
 }
 
 export default DBConnection;
